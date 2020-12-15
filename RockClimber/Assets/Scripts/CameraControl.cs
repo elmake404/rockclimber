2f5d661d28e0eaa85c739a4983aa764f3d2cc2d3
Valentin <kibirevvalik202@gmail.com>
2020-12-15 17:55:23 +0300

    [add] enemy

---
+++ RockClimber/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private Player _target;
+    private Vector3 _offSet, _cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    private float _topWindow = 5, _bottomWindow = 10;
+
+    void Start()
+    {
+        _offSet = _target.transform.position - transform.position;
+        _cameraPos = new Vector3(transform.position.x, _target.transform.position.y - _offSet.y, transform.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        if (_target.transform.position.y > transform.position.y + _topWindow)
+        {
+            _cameraPos = new Vector3(transform.position.x, _target.transform.position.y - _topWindow, transform.position.z);
+        }
+        else if (_target.transform.position.y < transform.position.y - _bottomWindow)
+        {
+            _cameraPos = new Vector3(transform.position.x, _target.transform.position.y + _bottomWindow, transform.position.z);
+        }
+
+        transform.position = Vector3.SmoothDamp(transform.position, _cameraPos, ref velocity, 0.07f);
+    }
+}
---
+++ RockClimber/Assets/Scripts/Turret.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem _fireFbx;
+    [SerializeField]
+    private Collider _colliderFire;
+
+    [SerializeField]
+    private float _timeFire=1, _timeFirePase=1;
+    void Start()
+    {
+        StartCoroutine(FireByFire());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator FireByFire()
+    {
+        while (true)
+        {
+            _fireFbx.Play();
+            _colliderFire.enabled = true;
+            yield return new WaitForSeconds(_timeFire);
+            _fireFbx.Stop();
+            _colliderFire.enabled = false;
+            yield return new WaitForSeconds(_timeFirePase);
+        }
+    }
+}
